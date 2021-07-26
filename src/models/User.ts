b7982508d54dfcbbfd1659cdf072baae2032feaa
Gal Levinshtein <10193612+Gkodkod@users.noreply.github.com>
2021-07-26 19:13:08 -0400

    Initialize renovate/sentry-webpack-plugin-1.x

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+import { TypeORMUserModel } from 'next-auth/adapters';
+import Adapters from 'next-auth/dist/adapters/index';
+
+export default class User extends (Adapters.TypeORM.Models.User.model as typeof TypeORMUserModel) {
+    constructor(name?: string, email?: string, image?: string, emailVerified?: Date) {
+        super(name, email, image, emailVerified);
+    }
+}
+
+export const UserSchema = {
+    name: 'User',
+    target: User,
+    columns: {
+        ...Adapters.TypeORM.Models.User.schema.columns,
+        dob: {
+            type: Date,
+            nullable: true,
+        },
+    },
+};
